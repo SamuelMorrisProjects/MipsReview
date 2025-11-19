5a0cdbff600df3421403bb7c96071e55fc8455f4
Samuel <samuelmorris333221@gmail.com>
2025-11-19 12:08:56 -0500

    Added a example of static linked list

---
+++ exam2/static_linkedlist.asm
@@ -0,0 +1,33 @@
+.text
+.globl main
+.ent main
+
+
+
+main:
+    la $s0, node1
+
+    print_loop: 
+        
+        li $v0, 1 
+        lw $a0, 0($s0)
+        syscall
+        lw   $t0, 4($s0)
+        move $s0, $t0
+
+        beqz $t0, end
+        j print_loop
+    
+    end: 
+        li $v0, 10 
+        syscall
+
+.end main
+
+
+
+.data
+# linked list static version 
+node1: .word 1  node2
+node2: .word 2  node3
+node3: .word 3  0
